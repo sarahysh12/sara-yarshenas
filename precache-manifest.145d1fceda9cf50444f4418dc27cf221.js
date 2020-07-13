b9669d07ca0f7ece9ffd7b8ce3a0e10aff97b93b
sarahysh12 <saraysh22@gmail.com>
2020-07-12 18:20:16 -0700

    Updates

---
+++ precache-manifest.145d1fceda9cf50444f4418dc27cf221.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b6f3adc5038ba50ab0c4bbd5f6ac2c",
+    "url": "/sara-yarshenas/index.html"
+  },
+  {
+    "revision": "22cbcf45d5fdb207547b",
+    "url": "/sara-yarshenas/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "b77f395ac63e06b59b04",
+    "url": "/sara-yarshenas/static/css/main.8d45d33f.chunk.css"
+  },
+  {
+    "revision": "22cbcf45d5fdb207547b",
+    "url": "/sara-yarshenas/static/js/2.c249d833.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sara-yarshenas/static/js/2.c249d833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b77f395ac63e06b59b04",
+    "url": "/sara-yarshenas/static/js/main.fc2f1f46.chunk.js"
+  },
+  {
+    "revision": "3dd550ee6e43697baf84",
+    "url": "/sara-yarshenas/static/js/runtime-main.21f47d4a.js"
+  },
+  {
+    "revision": "7c5c07e2f4ac5149123a470bfe717db0",
+    "url": "/sara-yarshenas/static/media/SY.7c5c07e2.png"
+  },
+  {
+    "revision": "a5982505936cf0f6029044825aa657cc",
+    "url": "/sara-yarshenas/static/media/aestheteMobile.a5982505.png"
+  },
+  {
+    "revision": "d6870f7387c92bfb128ae662eec937de",
+    "url": "/sara-yarshenas/static/media/aestheteWeb.d6870f73.png"
+  },
+  {
+    "revision": "ca90569beb04457b86ccd3bdd0f13c8f",
+    "url": "/sara-yarshenas/static/media/boat.ca90569b.jpg"
+  },
+  {
+    "revision": "4b72bd33eaa9e4380b4b39a5860d7656",
+    "url": "/sara-yarshenas/static/media/coffe.4b72bd33.jpeg"
+  },
+  {
+    "revision": "eadf90a5424cb7af742228b96305058f",
+    "url": "/sara-yarshenas/static/media/elham.eadf90a5.jpeg"
+  },
+  {
+    "revision": "ae210617b6730b27a0d88bf9f9f1469e",
+    "url": "/sara-yarshenas/static/media/fish.ae210617.jpeg"
+  },
+  {
+    "revision": "45db627915c085cb707dfeb9dc6d4431",
+    "url": "/sara-yarshenas/static/media/flamingo.45db6279.jpeg"
+  },
+  {
+    "revision": "d9ae85bdb91ab9976fc3beeead53fadf",
+    "url": "/sara-yarshenas/static/media/monster.d9ae85bd.jpg"
+  },
+  {
+    "revision": "917f2c97007c1f055e0f05cc88c2a113",
+    "url": "/sara-yarshenas/static/media/sara.917f2c97.jpg"
+  }
+]);
