857b1ae725c31b00c3e1eb5cd3912755e1af53d7
sarahysh12 <saraysh22@gmail.com>
2020-07-17 01:48:48 -0700

    update

---
+++ build/precache-manifest.defaccdb48379cf3e96a211f2db48535.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e13186f720e5f032d57126bb5117468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab1d52d548ce2deb6d9a",
+    "url": "/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "2b183c23d45d5b1a8db5",
+    "url": "/static/css/main.9c824f06.chunk.css"
+  },
+  {
+    "revision": "ab1d52d548ce2deb6d9a",
+    "url": "/static/js/2.0c13fc57.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0c13fc57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b183c23d45d5b1a8db5",
+    "url": "/static/js/main.88cb9d16.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  },
+  {
+    "revision": "f1f1efb4ed78d1ab1c9ddb6d28d0c20d",
+    "url": "/static/media/BGSUlogo.f1f1efb4.jpg"
+  },
+  {
+    "revision": "7c5c07e2f4ac5149123a470bfe717db0",
+    "url": "/static/media/SY.7c5c07e2.png"
+  },
+  {
+    "revision": "a5982505936cf0f6029044825aa657cc",
+    "url": "/static/media/aestheteMobile.a5982505.png"
+  },
+  {
+    "revision": "d6870f7387c92bfb128ae662eec937de",
+    "url": "/static/media/aestheteWeb.d6870f73.png"
+  },
+  {
+    "revision": "ca90569beb04457b86ccd3bdd0f13c8f",
+    "url": "/static/media/boat.ca90569b.jpg"
+  },
+  {
+    "revision": "4b72bd33eaa9e4380b4b39a5860d7656",
+    "url": "/static/media/coffe.4b72bd33.jpeg"
+  },
+  {
+    "revision": "a92d5035509e5e970051e5b1935c7ad1",
+    "url": "/static/media/dogCats.a92d5035.jpg"
+  },
+  {
+    "revision": "94a9dafb7d91c54d454666d912b910fb",
+    "url": "/static/media/elham.94a9dafb.jpeg"
+  },
+  {
+    "revision": "ae210617b6730b27a0d88bf9f9f1469e",
+    "url": "/static/media/fish.ae210617.jpeg"
+  },
+  {
+    "revision": "45db627915c085cb707dfeb9dc6d4431",
+    "url": "/static/media/flamingo.45db6279.jpeg"
+  },
+  {
+    "revision": "d9ae85bdb91ab9976fc3beeead53fadf",
+    "url": "/static/media/monster.d9ae85bd.jpg"
+  },
+  {
+    "revision": "917f2c97007c1f055e0f05cc88c2a113",
+    "url": "/static/media/sara.917f2c97.jpg"
+  },
+  {
+    "revision": "dc10ba16023fbd7f56321c841ed497dd",
+    "url": "/static/media/ship.dc10ba16.jpg"
+  },
+  {
+    "revision": "5163d4d9be6ffd3d34b550f26378f304",
+    "url": "/static/media/wordpress1.5163d4d9.png"
+  }
+]);
